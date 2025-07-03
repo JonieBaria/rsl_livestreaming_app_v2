c67a69da2991abe112a914a75e8c855032d3c87f
Jonie <joniebariaDE@gmail.com>
2025-07-03 19:50:53 +0800

    try personal

--- server.js
+++ server.js
@@ -52,7 +52,8 @@ wss.on("connection", (ws) => {
 
     "-f",
     "flv",
-    "rtmps://live-api-s.facebook.com:443/rtmp/FB-665053932562556-0-Ab1eyRCvkkMP4LJ3Wd6xIHiq",
+    "rtmps://live-api-s.facebook.com:443/rtmp/FB-2940425532795919-0-Ab0KAc1oWmmSkr1E3rheSKZF",
+    // "rtmps://live-api-s.facebook.com:443/rtmp/FB-665053932562556-0-Ab1eyRCvkkMP4LJ3Wd6xIHiq",
   ]);
 
   ffmpeg.stderr.on("data", (data) => console.log("FFmpeg:", data.toString()));
